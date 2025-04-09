4f85db3b4b2a84bfee7b1bed7144cfe99af11b7c
thrasosc <100732009+thrasosc@users.noreply.github.com>
2025-04-09 18:10:05 +0200

    Initial commit

---
+++ src/main/java/net/pixeldreamstudios/valor_mobs/ValorMobs.java
@@ -0,0 +1,10 @@
+package net.pixeldreamstudios.valor_mobs;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ValorMobs implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/net/pixeldreamstudios/valor_mobs/client/ValorMobsClient.java
@@ -0,0 +1,10 @@
+package net.pixeldreamstudios.valor_mobs.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ValorMobsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
