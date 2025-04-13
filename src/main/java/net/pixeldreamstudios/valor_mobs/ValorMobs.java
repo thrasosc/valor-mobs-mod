61abbcb49d6ee07d8a88678c245448018d908bc2
thrasosc <100732009+thrasosc@users.noreply.github.com>
2025-04-13 23:28:49 +0200

    remove cloth-config

--- src/main/java/net/pixeldreamstudios/valor_mobs/ValorMobs.java
+++ src/main/java/net/pixeldreamstudios/valor_mobs/ValorMobs.java
@@ -1,7 +1,5 @@
 package net.pixeldreamstudios.valor_mobs;
 
-import me.shedaniel.autoconfig.AutoConfig;
-import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer;
 import mod.azure.azurelib.common.internal.common.AzureLib;
 import net.fabricmc.api.ModInitializer;
 import net.pixeldreamstudios.valor_mobs.registry.CreativeModeTabRegistry;
@@ -13,12 +11,9 @@ import org.apache.logging.log4j.Logger;
 public class ValorMobs implements ModInitializer {
   public static final String MOD_ID = "valor_mobs";
   public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
-  public static ValorMobsConfig config;
 
   @Override
   public void onInitialize() {
-    AutoConfig.register(ValorMobsConfig.class, JanksonConfigSerializer::new);
-    config = AutoConfig.getConfigHolder(ValorMobsConfig.class).getConfig();
     AzureLib.initialize();
     EntityTypeRegistry.init();
     ItemRegistry.init();
